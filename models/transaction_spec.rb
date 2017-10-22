509e2b27f853cbd6870027c245d21c8152a20eda
Huascar <huascarmc@hotmail.com>
2017-10-22 13:34:27 +0100

    spec file for transaction, 1 assertion

---
+++ models/transaction_spec.rb
@@ -0,0 +1,17 @@
+require('minitest/autorun')
+require_relative('transaction.rb')
+
+class TestTransaction < MiniTest::Test
+
+  def setup
+    @transaction = Transaction.new({
+      'amount' => 200,
+      'tag' => 'food',
+      'shop' => 'tesco'
+    })
+  end
+
+  def test_save()
+   assert_equal(1, @transaction.save())
+  end
+end
