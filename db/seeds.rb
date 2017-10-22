37f4cc0aac90a8353e25a8ecffd0a3ac0fa5ca1c
Huascar <huascarmc@hotmail.com>
2017-10-22 13:06:28 +0100

    added sql_runner

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-require_relative('transaction.rb')
+require_relative('../models/transaction.rb')
+require_relative('../db/sql_runner.rb')
 
 transaction_1 = Transaction.new({
   'amount' => 200,
   'tag' => 'food',
   'shop' => 'tesco'
 })
+
+transaction_1.save()
--- models/transaction.rb
+++ models/transaction.rb
@@ -1,8 +1,17 @@
+require_relative('../db/sql_runner.rb')
+
 class Transaction
   def initialize(options)
     @id = options['id'].to_i
     @amount = options['amount'].to_i
     @tag = options['tag']
     @shop = options['shop']
+  end
+
+  def save()
+    sql = "INSERT INTO transactions (amount, tag, shop)
+     VALUES ($1, $2, $3) WHERE id = $1 RETURNING *"
+     values = [@amount, @tag, @shop]
+     @id = SqlRunner.run(sql, values)[0]['id'].to_i
   end
 end
