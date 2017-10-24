3ccb8b99f9baef600a22ddfba0f5b31de71c9d99
Huascar <huascarmc@hotmail.com>
2017-10-24 10:39:21 +0100

    users table

--- db/moneycashboard.sql
+++ db/moneycashboard.sql
@@ -1,9 +1,17 @@
 DROP TABLE transactions;
+DROP TABLE users;
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  budget INT4
+);
 
 CREATE TABLE transactions(
   id SERIAL PRIMARY KEY,
   amount INT4,
   tag VARCHAR(255),
   shop VARCHAR(255),
-  date DATE
+  date DATE,
+  user_id INT4 REFERENCES users(id)
 );
